e269df1ed48339a8e33b605231eabc85d53cfd77
engrusmanbelloa <usmanbelloa@gmail.com>
2024-11-22 06:16:18 +0100

    critters debug

--- next.config.js
+++ next.config.js
@@ -11,21 +11,21 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve = {
-                ...config.resolve,
-                fallback: {
-                    net: false,
-                    dns: false,
-                    tls: false,
-                    fs: false,
-                    request: false,
-                },
-            }
-        }
-        return config
-    },
+    // webpack: (config, { isServer }) => {
+    //     if (!isServer) {
+    //         config.resolve = {
+    //             ...config.resolve,
+    //             fallback: {
+    //                 net: false,
+    //                 dns: false,
+    //                 tls: false,
+    //                 fs: false,
+    //                 request: false,
+    //             },
+    //         }
+    //     }
+    //     return config
+    // },
 }
 
 module.exports = nextConfig
