98bdca48ac0342f2e6a4566e065bb5c5fa717732
engrusmanbelloa <usmanbelloa@gmail.com>
2025-06-19 12:05:49 +0100

    modules guest error

--- components/course/GuestCourseTab.tsx
+++ components/course/GuestCourseTab.tsx
@@ -6,12 +6,13 @@ import Tab from "@mui/material/Tab"
 import Box from "@mui/material/Box"
 import CourseModules from "@/components/course/CourseModules"
 import CertificateVerifier from "@/components/course/CertificateVerifier"
-import { CourseType, VideoSelectionProps } from "@/types"
+import { CourseType } from "@/types"
+import { mobile, ipad } from "@/responsive"
 
 const Container = styled(Box)`
     display: flex;
     flex-direction: column;
-    margin: 20px auto 0px;
+    margin: 10px auto 0px;
     padding: 0px;
 `
 const TabContainer = styled(Box)`
@@ -25,6 +26,18 @@ const TabLabel = styled(Tab)`
     font-weight: 500;
     color: ${(props) => props.theme.palette.common.black};
     text-transform: capitalize;
+`
+const ModuleTabLabel = styled(Tab)`
+    display: none;
+    font-size: 18px;
+    font-weight: 500;
+    color: ${(props) => props.theme.palette.common.black};
+    text-transform: capitalize;
+    ${ipad(
+        (props: any) => `
+            display: inline;
+        `,
+    )}
 `
 const SectionTitle = styled.h2`
     margin-bottom: 15px;
@@ -49,8 +62,8 @@ interface TabPanelProps {
 interface CourseProps {
     course: CourseType
     enrolled: boolean
-    setSelectedTitle: () => void
-    setSelectedVideo: () => void
+    setSelectedVideo: (url: string) => void
+    setSelectedTitle: (title: string) => void
 }
 
 function CustomTabPanel(props: TabPanelProps) {
@@ -68,7 +81,6 @@ function CustomTabPanel(props: TabPanelProps) {
         </div>
     )
 }
-
 function a11yProps(index: number) {
     return {
         id: `simple-tab-${index}`,
@@ -77,10 +89,13 @@ function a11yProps(index: number) {
 }
 
 // export default function BasicTabs({ courseDesc, whatYoullLearn, modules, review }: CourseProps) {
-export default function GuestCourseTab({ course, enrolled }: CourseProps) {
+export default function GuestCourseTabs({
+    course,
+    setSelectedTitle,
+    setSelectedVideo,
+    enrolled,
+}: CourseProps) {
     const [value, setValue] = useState(0)
-    const [selectedVideo, setSelectedVideo] = useState<string | null>(null)
-    const [selectedTitle, setSelectedTitle] = useState<string | null>(null)
 
     const handleChange = (event: SyntheticEvent, newValue: number) => {
         setValue(newValue)
@@ -92,13 +107,18 @@ export default function GuestCourseTab({ course, enrolled }: CourseProps) {
                 <TabsItem value={value} onChange={handleChange} aria-label="basic tabs example">
                     <TabLabel label="Modules" {...a11yProps(0)} />
                     <TabLabel label="Course detail" {...a11yProps(1)} />
-                    <TabLabel label="Student Reviews" {...a11yProps(2)} />
+                    <TabLabel label="Certificate" {...a11yProps(2)} />
                 </TabsItem>
             </TabContainer>
             {/* Course detail section */}
             <CustomTabPanel value={value} index={0}>
                 <SectionTitle>Course Modules</SectionTitle>
-                <CourseModules enrolled={enrolled} course={course} />
+                <CourseModules
+                    setSelectedTitle={setSelectedTitle}
+                    setSelectedVideo={setSelectedVideo}
+                    enrolled={enrolled}
+                    course={course}
+                />
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
                 <SectionTitle>About Course</SectionTitle>
@@ -110,15 +130,8 @@ export default function GuestCourseTab({ course, enrolled }: CourseProps) {
                     </BulletList>
                 ))}
             </CustomTabPanel>
-            <CustomTabPanel value={value} index={3}>
-                {course.review.map((item, i) => (
-                    <div key={i}>
-                        <p>
-                            <strong>{item.Name}</strong> - {item.stars} stars
-                        </p>
-                        <p>{item.comment}</p>
-                    </div>
-                ))}
+            <CustomTabPanel value={value} index={2}>
+                <CertificateVerifier />
             </CustomTabPanel>
         </Container>
     )
