9a9fffd00b7bf136685f9d7c3f95dcdc09878bd8
RoamX <usmanbelloa@gmail.com>
2025-10-22 11:00:38 +0100

    btn-zindenx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,6 @@
 import HomeComponent from "@/components/home/Home"
 
 export default async function Home() {
-    // useEffect(() => {
-    //     setIsLoading(true)
-    //     async function fetchCourses() {
-    //         const response = await fetch("/api/courses/getCourses")
-    //         const data = await response.json()
-    //         setCourses(data)
-    //         setCount(data.count)
-    //         setIsLoading(false)
-    //         console.log("courses found: ", courses)
-    //     }
-    //     fetchCourses()
-    // }, [])
-    const limit = 3
-
-    // if (isLoading) {
-    //   return <IsLoading />
-    // }
-
     return (
         <>
             <HomeComponent />
