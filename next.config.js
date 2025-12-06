42fd90fc8350ee648f2e726c4bbbe1f09928a2f9
RoamX <usmanbelloa@gmail.com>
2025-12-06 19:37:07 +0100

    upgrade buid upgrade

--- next.config.js
+++ next.config.js
@@ -1,37 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
-//     transpilePackages: [
-//     '@mui/material',
-//     '@mui/system',
-//     '@mui/icons-material', // If @mui/icons-material is being used
-//   ],
-//     reactStrictMode: true,
-
-//     compiler: {
-//         styledComponents: true,
-//     },
-//     experimental: {
-//         // optimizeCss: true,
-//     },
-//     images: {
-//         unoptimized: true,
-//     },
-    
-//     webpack: (config, { isServer }) => {
-//         if (!isServer) {
-//             config.resolve.alias = {
-//                 ...config.resolve.alias,
-//                 '@mui/styled-engine': '@mui/styled-engine-sc',
-//             }
-//         }
-//         return config
-//     },
-// }
-// // });
-
-// module.exports = nextConfig
-
 const nextConfig = {
     transpilePackages: [
         '@mui/material',
