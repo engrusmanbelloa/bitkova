5facc3fda433e4effbae554ba66cffa37bb96664
RoamX <usmanbelloa@gmail.com>
2026-01-20 07:58:57 +0100

    dynamic classes

---
+++ lib/telegram/renderers/renderClassButtons.ts
@@ -0,0 +1,27 @@
+export function renderClassButtons({ cohort, telegramClasses, physicalClasses }: any) {
+    const buttons: any[] = []
+
+    telegramClasses.forEach((c: any) => {
+        buttons.push([
+            {
+                text: `💳 Enroll: ${c.name}`,
+                url: `https://bitkova.com/pay/telegram/${c.id}`,
+            },
+        ])
+    })
+
+    physicalClasses.forEach((c: any) => {
+        buttons.push([
+            {
+                text: `📍 Enroll: ${c.name}`,
+                url: `https://bitkova.com/pay/physical/${c.id}`,
+            },
+        ])
+    })
+
+    return {
+        reply_markup: {
+            inline_keyboard: buttons,
+        },
+    }
+}
