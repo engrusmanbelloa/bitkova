ffacf244c788124cb29cd32b9a88d0cbc0f6093a
RoamX <usmanbelloa@gmail.com>
2026-03-27 09:21:51 +0100

    metadata img url full

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
         siteName: "Bitkova Digital Hub Ltd",
         images: [
             {
-                url: "/slider/2.jpg",
+                url: "https://bitkova.com/slider/2.jpg",
                 width: 1200,
                 height: 630,
                 alt: "Bitkova Digital Hub Ltd",
@@ -27,17 +27,10 @@ export const metadata: Metadata = {
         card: "summary_large_image",
         title: "Bitkova Digital Hub Ltd | Empowering the Next Generation of African Tech Talent",
         description: "Join the next cohort of engineers in Arewa.",
-        images: ["/slider/2.jpg"],
+        images: ["https://bitkova.com/slider/2.jpg"],
     },
 }
 
 export default async function Home() {
     return <HomeComponent />
 }
-
-// // app/page.tsx
-// import HomeComponent from "@/components/home/Home"
-
-// export default async function Home() {
-//     return <HomeComponent />
-// }
